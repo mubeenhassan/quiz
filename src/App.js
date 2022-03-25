590b62739b2b1069d02639c5d31bd25d461a90a0
mubeenhassan <mubeenhassan34@gmail.com>
2022-03-25 17:34:23 +0500

    adding react router and fixing radio button calculation bug

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import './App.css';
-import Home from "./components/Home";
+import React from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
+import './App.css'
+import Home from './components/Home'
+import Result from './components/Result/'
 
-function App()  {
-    return (
-      <div className="App">
-        <Home />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/result' element={<Result />} />
+      </Routes>
+    </div>
+  )
+}
 
-
-export default App;
+export default App
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,34 +1,35 @@
-import React, { useState, useEffect } from "react"
-import { QUIZ } from "../DataFolder/QuizData"
-import QuestionComponent from "./QuestionComponent"
-import ProgressBar from "../Reused/ProgressBar"
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { QUIZ } from '../DataFolder/QuizData'
+import QuestionComponent from './QuestionComponent'
+import ProgressBar from '../Reused/ProgressBar'
 
-import step1 from "../../assets/images/step-icon-01.png"
-import step2 from "../../assets/images/step-icon-02.png"
-import step3 from "../../assets/images/step-icon-03.png"
-import arrow from "../../assets/images/arrow.png"
+import step1 from '../../assets/images/step-icon-01.png'
+import step2 from '../../assets/images/step-icon-02.png'
+import step3 from '../../assets/images/step-icon-03.png'
+import arrow from '../../assets/images/arrow.png'
 
-const QUESTIONS_PER_PAGE = 8;
-const TOTAL_QUESTIONS = QUIZ.length;
+const QUESTIONS_PER_PAGE = 8
+const TOTAL_QUESTIONS = QUIZ.length
 const TOTAL_PAGES = TOTAL_QUESTIONS / QUESTIONS_PER_PAGE
 const STEP_DATA = [
   {
     icon: step1,
-    heading: "Complete The Test",
+    heading: 'Complete The Test',
     description:
-      "Take your time and answer honestly to find out the dominant strand of your trauma bond",
+      'Take your time and answer honestly to find out the dominant strand of your trauma bond',
   },
   {
     icon: step2,
-    heading: "View Your Results",
+    heading: 'View Your Results',
     description:
-      "Learn how your trauma bond is showing up and what you need to do to start breaking it",
+      'Learn how your trauma bond is showing up and what you need to do to start breaking it',
   },
   {
     icon: step3,
-    heading: "Unlock Your Potential",
+    heading: 'Unlock Your Potential',
     description:
-      "Take the first steps towards freedom with a 3-step strategy tailored to your trauma bond and subconscious processing style",
+      'Take the first steps towards freedom with a 3-step strategy tailored to your trauma bond and subconscious processing style',
   },
 ]
 function Home() {
@@ -40,7 +41,7 @@ function Home() {
     EM: 0,
     KI: 0,
     AU: 0,
-    VI: 0
+    VI: 0,
   })
 
   useEffect(() => {
@@ -52,41 +53,44 @@ function Home() {
     if (currentPage <= TOTAL_PAGES) {
       let quizData = [...QUIZ]
       let start = QUESTIONS_PER_PAGE * currentPage
-      let end = (QUESTIONS_PER_PAGE * (currentPage + 1) - 1)
+      let end = QUESTIONS_PER_PAGE * (currentPage + 1) - 1
       quizData = quizData.slice(start, end + 1)
       setQuiz(quizData)
       setCurrentPage(currentPage + 1)
     }
   }
 
-  const handleFinish=()=>{
-    alert("Quiz Done")
-  }
-
   const renderQuestions = () => {
     return quiz.map((questions, key) => {
-      let questionNumber = (((currentPage - 1) * QUESTIONS_PER_PAGE + key) + 1)
-      return <QuestionComponent
-        key={key}
-        questionID={key}
-        data={questions}
-        questionNumber={questionNumber}
-        points={points}
-        setPoints={setPoints}
-      />
+      let questionNumber = (currentPage - 1) * QUESTIONS_PER_PAGE + key + 1
+      return (
+        <QuestionComponent
+          key={key}
+          questionID={key}
+          data={questions}
+          questionNumber={questionNumber}
+          points={points}
+          setPoints={setPoints}
+        />
+      )
     })
   }
   return (
-    <div className="main-container">
-    <ProgressBar bgcolor={"#e8c0aa"} completed={parseInt((QUESTIONS_PER_PAGE/TOTAL_QUESTIONS)*100 * currentPage)} />
-      <div className="heading-container">
+    <div className='main-container'>
+      <ProgressBar
+        bgcolor={'#e8c0aa'}
+        completed={parseInt(
+          (QUESTIONS_PER_PAGE / TOTAL_QUESTIONS) * 100 * (currentPage-1)
+        )}
+      />
+      <div className='heading-container'>
         <h1>Free Trauma Bond Test</h1>
         <p>Find out which type of trauma bond you have and how to break it</p>
       </div>
-      <div className="step-container">
+      <div className='step-container'>
         {STEP_DATA.map((item, key) => (
-          <div className="step-card" key={key}>
-            <div className="step-icon">
+          <div className='step-card' key={key}>
+            <div className='step-icon'>
               <img src={item.icon} />
             </div>
             <h2>{item.heading}</h2>
@@ -94,19 +98,22 @@ function Home() {
           </div>
         ))}
       </div>
-      <div className="question-container">
+      <div className='question-container'>
         {renderQuestions()}
         {currentPage <= TOTAL_PAGES ? (
-          <button onClick={handleNext} className="next-button">
-          <span>Next</span>
-          <img src={arrow} alt="" />
-        </button>
-        ):(
-          <button onClick={handleFinish} className="next-button">
-          <span>Finish Quiz</span>
-          <img src={arrow} alt="" />
-        </button>
-        )}        
+          <button onClick={handleNext} className='next-button'>
+            <span>Next</span>
+            <img src={arrow} alt='' />
+          </button>
+        ) : (
+          <Link to='/result'>
+            <div className='next-button'>
+              <span>Finish Quiz</span>
+
+              <img src={arrow} alt='' />
+            </div>
+          </Link>
+        )}
       </div>
     </div>
   )
