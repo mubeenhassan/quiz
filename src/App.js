c1831a5ebd9ab6b0d0b9d13f26ded16ae570bef3
mubeenhassan <mubeenhassan34@gmail.com>
2022-03-25 19:28:45 +0500

    removing react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 import Home from './components/Home'
-import Result from './components/Result/'
 
 function App() {
   return (
     <div className='App'>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/result' element={<Result />} />
-      </Routes>
+      <Home />
     </div>
   )
 }
